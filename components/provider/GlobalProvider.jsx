834451cce87af30c05f65ca118bd423661b51e39
sakib-repliq <sakibul.islam@repliq.dev>
2024-04-17 16:57:38 +0600

    Add new dependencies and update global styles

--- components/provider/GlobalProvider.jsx
+++ components/provider/GlobalProvider.jsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "../ui/sonner";
 
 export default function GlobalProvider({ children }) {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Toaster position="bottom-right" richColors />
+      {children}
+    </QueryClientProvider>
   );
 }
